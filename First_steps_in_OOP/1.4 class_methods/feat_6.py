d31e7ec9485b039d1d800acf52761ded552589f5
3dendroid <denkingbad@gmail.com>
2024-10-13 18:16:11 +0700

    next feat_9.py

---
+++ First_steps_in_OOP/1.4 class_methods/feat_6.py
@@ -0,0 +1,8 @@
+class Stepik:
+    def next_task(self):
+        return "Следующее задание"
+
+my_st = Stepik ()
+
+print(my_st)
+# print(my_st.next_task())
---
+++ First_steps_in_OOP/1.4 class_methods/feat_8.py
@@ -0,0 +1,17 @@
+class String:
+    is_empty = False
+
+
+s1 = String ()
+s2 = String ()
+
+s2.is_empty = True
+
+a = s1.is_empty
+b = s2.is_empty
+
+print (a)
+print (b)
+print(String.is_empty)
+
+
