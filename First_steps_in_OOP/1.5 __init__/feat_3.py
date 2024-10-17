582c53ec182b0c5aa825fc73203022994d7a6dc2
3dendroid <denkingbad@gmail.com>
2024-10-17 14:59:40 +0700

    next feat_5.py in 1.5 __init__

--- First_steps_in_OOP/1.5 __init__/feat_3.py
+++ First_steps_in_OOP/1.5 __init__/feat_3.py
@@ -1,1 +1,9 @@
-# next
+class Point:
+    def __init__(self, x, y, color='black'):
+        self.x = x
+        self.y = y
+        self.color = color
+
+
+points = [Point (i, i) for i in range (1, 2000, 2)]
+points[1].color = 'yellow'
