71e5e5a450bd8d3614360e2848168f1a0b9f2c7f
3dendroid <denkingbad@gmail.com>
2024-10-19 14:58:27 +0700

    next feat_6.py

---
+++ First_steps_in_OOP/1.5 __init__/feat_6.py
@@ -0,0 +1,11 @@
+class Graph:
+    def __init__(self, data, is_show=True):
+        self.data = data[:]
+
+
+# считывание списка из входного потока (эту строку не менять)
+data_graph = list (map (int, input ().split ()))
+
+gr_1 = Graph (data)
+
+# next
