d324ca7c715d33c0516058ea33044bdc3b33a5a0
3dendroid <denkingbad@gmail.com>
2024-10-23 14:56:17 +0700

    next feat_8.py

--- First_steps_in_OOP/1.5 __init__/feat_7.py
+++ First_steps_in_OOP/1.5 __init__/feat_7.py
@@ -1,10 +1,27 @@
 class CPU:
-    pass
+    def __init__(self, name, fr):
+        self.name = name
+        self.fr = fr
 
 
 class Memory:
-    pass
+    def __init__(self, name, volume):
+        self.name = name
+        self.volume = volume
 
 
 class MotherBoard:
-    pass
+    def __init__(self, name, cpu, *mems):
+        self.name = name
+        self.cpu = cpu
+        self.total_mem_slots = 4
+        self.mem_slots = mems[:self.total_mem_slots]
+
+    def get_config(self):
+        return [f'Материнская плата:{self.name}',
+                f'Центральный процессор: {self.cpu.name}, {self.cpu.fr}',
+                f'Слотов памяти: {self.total_mem_slots}',
+                f'Память: {"; ".join (f"{item.name} - {item.volume}" for item in self.mem_slots)}']
+
+
+mb = MotherBoard ('GigaByte', CPU ('Intel', 2000), Memory ('Kingston', 3600), Memory ('Kingston', 3600))
