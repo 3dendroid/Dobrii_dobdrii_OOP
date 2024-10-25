4e6cc9fa5cdba7622806acfae8a0ea201733da01
3dendroid <denkingbad@gmail.com>
2024-10-25 15:42:08 +0700

    next feat_9.py

--- First_steps_in_OOP/1.5 __init__/feat_8.py
+++ First_steps_in_OOP/1.5 __init__/feat_8.py
@@ -1,1 +1,47 @@
-# next
+class Cart:
+
+    def __init__(self):
+        self.goods = []
+
+    def add(self, gd):
+        self.goods.append (gd)
+
+    def remove(self, indx):
+        self.goods.pop (indx)
+
+    def get_list(self):
+        return [f'{x.name}: {x.price}' for x in self.goods]
+
+
+class Table:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class TV:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Notebook:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Cup:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+cart = Cart ()
+cart.add (TV ('Xiaomi', 1100))
+cart.add (TV ('Sony', 2000))
+cart.add (Table ('Gurdgen', 1700))
+cart.add (Notebook ('Gigabyte', 3000))
+cart.add (Notebook ('MSI', 3500))
+
+print (cart.get_list ())
