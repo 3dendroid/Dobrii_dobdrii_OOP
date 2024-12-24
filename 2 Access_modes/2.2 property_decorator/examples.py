4b6bad24906025498a4d767cf8b129a13d170a99
3dendroid <denkingbad@gmail.com>
2024-12-24 13:17:51 +0700

    next feat_4.py

--- 2 Access_modes/2.2 property_decorator/examples.py
+++ 2 Access_modes/2.2 property_decorator/examples.py
@@ -1,1 +1,22 @@
-# next
+class Person:
+    def __init__(self, name, old):
+        self.__name = name
+        self.__old = old
+
+    @property
+    def old(self):
+        return self.__old
+
+    @old.setter
+    def old(self, old):
+        self.__old = old
+
+    @old.deleter
+    def old(self):
+        del self.__old
+
+
+p = Person ('Denis', 22)
+del p.old
+# p.get_old = 31
+print (p.__dict__)
---
+++ 2 Access_modes/2.2 property_decorator/feat_1.py
@@ -0,0 +1,15 @@
+class Money:
+    def __init__(self):
+        self.__money = 0
+
+    def set_money(self, value):
+        self.__money = value
+
+    def get_money(self):
+        return self.__money
+
+    money = property (get_money, set_money)
+
+
+m = Money ()
+m.money = 10
