e77704e7ba68196c24972fca14afd6c034c2fe44
3dendroid <denkingbad@gmail.com>
2025-01-01 17:49:56 +0700

    next feat_5.py

--- 2 Access_modes/2.2 property_decorator/feat_4.py
+++ 2 Access_modes/2.2 property_decorator/feat_4.py
@@ -1,1 +1,12 @@
-# next
+class Car:
+    def __init__(self):
+        self.model = None
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, value):
+        if type (value) == str and 2 <= len (value) <= 100:
+            self.__model = value
