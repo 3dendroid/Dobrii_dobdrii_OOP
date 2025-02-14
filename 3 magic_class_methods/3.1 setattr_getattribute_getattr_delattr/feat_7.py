83d7b8bcce959ba523b4dd534f5fa976372b831b
Denis Sazonov <denis.sazonov@mytona.com>
2025-02-14 19:05:24 +0700

    NEXT: feat_9.py

--- 3 magic_class_methods/3.1 setattr_getattribute_getattr_delattr/feat_7.py
+++ 3 magic_class_methods/3.1 setattr_getattribute_getattr_delattr/feat_7.py
@@ -1,1 +1,29 @@
-# NEXT
+class SmartPhone:
+    def __init__(self, model):
+        self.model = model
+        self.apps = []
+
+    def add_app(self, app):
+        if len(tuple(filter(lambda x: type(x) == type(app), self.apps))) == 0:
+            self.apps.append(app)
+
+    def remove_app(self, app):
+        if app in self.apps:
+            self.apps.remove(app)
+
+
+class AppVK:
+    def __init__(self):
+        self.name = "Вконтакте"
+
+
+class AppYouTube:
+    def __init__(self, memory_max):
+        self.name = "Вконтакте"
+        self.memory_max = memory_max
+
+
+class AppPhone:
+    def __init__(self, phones):
+        self.name = "Phone"
+        self.phone_list = phones
