382e85a7d608423b2e9872fab47675f781f835ca
Denis Sazonov <denis.sazonov@mytona.com>
2025-02-24 14:22:10 +0700

    NEXT: feat_2.py

--- 3 magic_class_methods/3.2 __call__/examples.py
+++ 3 magic_class_methods/3.2 __call__/examples.py
@@ -1,1 +1,34 @@
-# NEXT
+import math
+
+
+# class StripChars:
+#     def __init__(self, chars):
+#         self.__counter = 0
+#         self.__chars = chars
+#
+#     def __call__(self, *args, **kwargs):
+#         if not isinstance(args[0], str):
+#             raise TypeError("Неверный тип присваиваемых данных.")
+#         return args[0].strip(self.__chars)
+#
+#
+# s1 = StripChars("!@#$%^&*() -_=+[]{};:,.<>/?")
+# s2 = StripChars(" ")
+# res = s1(" Hello World! @%!")
+# res2 = s2("     H ell oW or ld! ")
+# print(res, res2, sep="\n")
+
+
+class Derivate:
+    def __init__(self, func):
+        self.__fn = func
+
+    def __call__(self, x, dx=0.0001, *args, **kwargs):
+        return (self.__fn(x + dx) - self.__fn(x)) / dx
+
+@Derivate
+def df_sin(x):
+    return math.sin(x)
+
+# df_sin = Derivate(df_sin)
+print(df_sin(math.pi / 3))
