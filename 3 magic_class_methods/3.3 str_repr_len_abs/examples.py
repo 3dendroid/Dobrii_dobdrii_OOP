37ce42aa1646016401ff497d8ba87e7a07e00e84
denis sazonov <denis.sazonov@mytona.com>
2025-03-14 14:36:53 +0700

    NEXT: feat_2.py

--- 3 magic_class_methods/3.3 str_repr_len_abs/examples.py
+++ 3 magic_class_methods/3.3 str_repr_len_abs/examples.py
@@ -1,1 +1,24 @@
-# NEXT
+# class Cat:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def __repr__(self):
+#         return f'{self.__class__}: {self.name})'
+#
+#     def __str__(self):
+#         return f'{self.name}'
+from cgi import print_environ_usage
+
+
+class Point:
+    def __init__(self, *args):
+        self.__coords = args
+    def __len__(self):
+        return len(self.__coords)
+    def __abs__(self):
+        return list(map(abs, self.__coords))
+
+p = Point(1, 2, 3)
+print(len(p))
+print(abs(p))
+
