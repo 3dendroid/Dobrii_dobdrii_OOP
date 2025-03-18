f1dca93523b5ce4e044d78392da11e37be5f5c20
denis sazonov <denis.sazonov@mytona.com>
2025-03-18 10:27:50 +0700

    NEXT: feat_3.py

--- 3 magic_class_methods/3.3 str_repr_len_abs/feat_2.py
+++ 3 magic_class_methods/3.3 str_repr_len_abs/feat_2.py
@@ -1,1 +1,16 @@
-# NEXT
+import sys
+
+
+class Book:
+    def __init__(self, title: str, author: str, pages: int):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f'Книга: {self.title}; {self.author}; {self.pages}'
+
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+book = Book(lst_in[0], lst_in[1], int(lst_in[2]))
+print(book)
