6960f01a512584500d251a5d8fb5cc5b4ff1bd81
denis sazonov <denis.sazonov@mytona.com>
2025-03-20 10:24:46 +0700

    NEXT: feat_4.py

--- 3 magic_class_methods/3.3 str_repr_len_abs/feat_3.py
+++ 3 magic_class_methods/3.3 str_repr_len_abs/feat_3.py
@@ -1,1 +1,16 @@
-# NEXT
+class Model:
+    def __init__(self):
+        self.fields = {}
+
+    def query(self, **kwargs):
+        self.fields.update(kwargs)
+
+    def __str__(self):
+        if self.fields:
+            return "Model: " + ", ".join(f"{key}={value}" for key, value in self.fields.items())
+        return "Model"
+
+
+# model = Model()
+# model.query(id=1, fio='Sergey', old=33)
+# print(model)
