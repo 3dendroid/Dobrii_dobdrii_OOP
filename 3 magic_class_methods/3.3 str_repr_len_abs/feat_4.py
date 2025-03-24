ed9a8e7ae71535d6a26f9fb063faabc4bb0bc3cc
denis sazonov <denis.sazonov@mytona.com>
2025-03-24 10:21:23 +0700

    NEXT: feat_5.py

--- 3 magic_class_methods/3.3 str_repr_len_abs/feat_4.py
+++ 3 magic_class_methods/3.3 str_repr_len_abs/feat_4.py
@@ -1,1 +1,29 @@
-# NEXT
+class WordString:
+    def __init__(self, string=""):
+        self._string = string
+
+    @property
+    def string(self):
+        return self._string
+
+    @string.setter
+    def string(self, value):
+        self._string = value
+
+    def __len__(self):
+        return len(self._string.split())
+
+    def __call__(self, indx, *args, **kwargs):
+        words = self._string.split()
+        if 0 <= indx < len(words):
+            return words[indx]
+        else:
+            return IndexError("Индекс вне диапазона слов")
+
+
+words = WordString()
+words.string = "Курс по Python ООП"
+n = len(words)
+first = "" if n == 0 else words(0)
+print(words.string)
+print(f"Число слов: {n}; первое слово: {first}")
