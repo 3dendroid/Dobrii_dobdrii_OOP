1d41590aaf18772e3ddd7cacf3b2be9845331c7c
denis sazonov <denis.sazonov@mytona.com>
2025-04-07 16:21:57 +0700

    NEXT: examples.py in folder 3.4

--- 3 magic_class_methods/3.3 str_repr_len_abs/feat_10.py
+++ 3 magic_class_methods/3.3 str_repr_len_abs/feat_10.py
@@ -1,1 +1,12 @@
-# NEXT
+class PolyLine:
+    def __init__(self, *coords):
+        self._coords = list(coords)
+
+    def add_coord(self, x, y):
+        self._coords.append((x, y))
+
+    def remove_coord(self, indx):
+        del self._coords[indx]
+
+    def get_coords(self):
+        return self._coords
