77857c6b8fc3e67713a662a6fd39a51ce2983b99
denis sazonov <denis.sazonov@mytona.com>
2025-04-25 12:38:22 +0700

    NEXT: feat_9.py

--- 3 magic_class_methods/3.4 __add__ and __sub__/feat_8.py
+++ 3 magic_class_methods/3.4 __add__ and __sub__/feat_8.py
@@ -1,1 +1,26 @@
-# NEXT
+class Item:
+    def __init__(self, name, money):
+        self.name = name
+        self.money = money
+
+    def __add__(self, other):
+        if not isinstance(other, Item) and not isinstance(other.money, (int, float)):
+            raise AttributeError("Неправильный тип данных")
+        self.money += other.money
+        return self.money
+
+    def __radd__(self, other):
+        return other + self.money
+
+
+class Budget:
+    lst_it = []
+
+    def add_item(self, it):
+        self.lst_it.append(it.money)
+
+    def remove_item(self, indx):
+        del self.lst_it[indx]
+
+    def get_items(self):
+        return self.lst_it
