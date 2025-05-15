ca1f2461cc0db1f4e75613e748c1621dead1ae2a
denis sazonov <denis.sazonov@mytona.com>
2025-05-15 15:15:07 +0700

    NEXT: feat_6.py

--- 3 magic_class_methods/3.5 __eq__, __ne__/feat_5.py
+++ 3 magic_class_methods/3.5 __eq__, __ne__/feat_5.py
@@ -1,1 +1,27 @@
-# NEXT
+class StringText:
+    def __init__(self, lst):
+        self.lst_words = list(lst)
+
+    def __len__(self):
+        return len(self.lst_words)
+
+    def __lt__(self, other):
+        return len(self) < len(other)
+
+    def __le__(self, other):
+        return len(self) <= len(other)
+
+
+stich = ["Я к вам пишу – чего же боле?",
+         "Что я могу еще сказать?",
+         "Теперь, я знаю, в вашей воле",
+         "Меня презреньем наказать.",
+         "Но вы, к моей несчастной доле",
+         "Хоть каплю жалости храня,",
+         "Вы не оставите меня."]
+
+strip_chars = "–?!,.;"
+lst_text = [StringText(word.strip("–?!,.;") for word in words) for words in (line.split() for line in stich)]
+lst_text_sorted = sorted(lst_text, reverse=True)
+lst_text_sorted = [' '.join(x.lst_words) for x in lst_text_sorted]
+print(lst_text_sorted)
