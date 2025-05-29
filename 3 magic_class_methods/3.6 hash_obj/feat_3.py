de7a1fbaf18fd1ba122337d2373a0be521f01db7
denis sazonov <denis.sazonov@mytona.com>
2025-05-29 10:58:50 +0700

    NEXT: feat_5.py

--- 3 magic_class_methods/3.6 hash_obj/feat_3.py
+++ 3 magic_class_methods/3.6 hash_obj/feat_3.py
@@ -1,1 +1,13 @@
-# NEXT
+class PathLine:
+    def __init__(self, dist, angle):
+        self.dist = dist
+        self.angle = angle
+
+    def __eq__(self, other):
+        return abs(self.dist) == abs(other.dist)
+
+p1 = PathLine(10, 1.57)
+p2 = PathLine(-10, 0.49)
+
+h1, h2 = hash(p1), hash(p2)
+print(h1 == h2)
