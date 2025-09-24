e12a979947ece808dc1027f595b13387724428e8
denis.sazonov <denis.sazonov@mytona.com>
2025-09-24 15:38:08 +0700

    NEXT: feat_8.py

--- 3 magic_class_methods/3.9 __iter__, __next__/feat_7.py
+++ 3 magic_class_methods/3.9 __iter__, __next__/feat_7.py
@@ -1,1 +1,8 @@
-# NEXT
+class IterColumn:
+    def __init__(self, lst, col_indx):
+        self._lst = lst
+        self._col_indx = col_indx
+
+    def __iter__(self):
+        for row in self._lst:
+            yield row[self._col_indx]
