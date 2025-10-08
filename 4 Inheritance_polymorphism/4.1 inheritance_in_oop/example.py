3050a162e34d01b01bd1d6a361b09f23d27e8482
denis.sazonov <denis.sazonov@mytona.com>
2025-10-08 09:06:48 +0700

    NEXT: feat_4.py

--- 4 Inheritance_polymorphism/4.1 inheritance_in_oop/example.py
+++ 4 Inheritance_polymorphism/4.1 inheritance_in_oop/example.py
@@ -1,1 +1,31 @@
-# NEXT
+class Geom:
+    name = 'Geom'
+
+    def set_coords(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def draw(self):
+        print('Primitive draw')
+
+
+class Line(Geom):
+    name = 'Line'
+    def draw(self):
+        print('Line draw')
+
+
+class Rect(Geom):
+    def draw(self):
+        print('Rectangle draw')
+    # pass
+
+
+l = Line()
+r = Rect()
+l.draw()
+r.draw()
+print(l.name)
+print(r.name)
---
+++ 4 Inheritance_polymorphism/4.1 inheritance_in_oop/feat_3.py
@@ -0,0 +1,30 @@
+class Table:
+    def __init__(self, model, color):
+        self.set_model_and_color(model, color)
+
+    def set_model_and_color(self, model, color):
+        self.model = model
+        self.color = color
+
+
+class RoundTable(Table):
+    def __init__(self, model, color, radius, height):
+        super().__init__(model, color)
+        self.radius = radius
+        self.height = height
+
+    def set_params(self, model, color, radius, height):
+        self.model = model
+        self.color = color
+        self.radius = radius
+        self.height = height
+
+
+rt = RoundTable('PC', 'brown', 500, 750)
+tb = Table('Home', 'white')
+color = rt.color
+# h = tb.height
+rt.set_params('Home', 'red', 700, 1000)
+# tb.set_params('PC', 'brown', 500, 750)
+rt.set_model_and_color('Home', 'red')
+m = tb.model
