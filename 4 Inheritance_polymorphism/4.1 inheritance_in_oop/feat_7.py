04d2a986715c144574e9c4292770ee249e980a7f
denis.sazonov <denis.sazonov@mytona.com>
2025-10-20 16:41:09 +0700

    NEXT: feat_8.py

--- 4 Inheritance_polymorphism/4.1 inheritance_in_oop/feat_7.py
+++ 4 Inheritance_polymorphism/4.1 inheritance_in_oop/feat_7.py
@@ -1,1 +1,23 @@
-# NEXT
+class Singleton:
+    _instance = None
+    _instance_base = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls == Singleton:
+            if cls._instance_base is None:
+                cls._instance_base = object.__new__(cls)
+            return cls._instance_base
+
+        if not cls._instance:
+            cls._instance = object.__new__(cls)
+
+        return cls._instance
+
+
+class Game(Singleton):
+    def __init__(self, name):
+        if 'name' not in self.__dict__:
+            self.name = name
+
+
+
