27c569af80f670caa49f19f9da9239aaa582230e
denis.sazonov <denis.sazonov@mytona.com>
2025-10-22 12:22:48 +0700

    NEXT: feat_9.py

--- 4 Inheritance_polymorphism/4.1 inheritance_in_oop/feat_8.py
+++ 4 Inheritance_polymorphism/4.1 inheritance_in_oop/feat_8.py
@@ -1,1 +1,36 @@
-# NEXT
+class Validator:
+    def _is_valid(self, data):
+        # Базовый валидатор всегда принимает данные
+        return True
+
+    def __call__(self, data):
+        if self._is_valid(data):
+            return data
+        raise ValueError('данные не прошли валидацию')
+
+
+class IntegerValidator(Validator):
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def _is_valid(self, data):
+        # Отвергаем bool, т.к. bool является подклассом int
+        if isinstance(data, bool):
+            return False
+        if not isinstance(data, int):
+            return False
+        return self.min_value <= data <= self.max_value
+
+
+class FloatValidator(Validator):
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def _is_valid(self, data):
+        # Требуем именно float (int не принимаем)
+        if not isinstance(data, float):
+            return False
+        return self.min_value <= data <= self.max_value
+
