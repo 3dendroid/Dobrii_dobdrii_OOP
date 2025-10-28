2c59ed48344d3c16a91655268957912652d9d646
denis.sazonov <denis.sazonov@mytona.com>
2025-10-28 12:14:51 +0700

    NEXT: folder 4.2

--- 4 Inheritance_polymorphism/4.1 inheritance_in_oop/feat_10.py
+++ 4 Inheritance_polymorphism/4.1 inheritance_in_oop/feat_10.py
@@ -1,1 +1,46 @@
-# NEXT
+class Vector:
+    _allowed_types = (int, float)
+
+    def __init__(self, *coords):
+        self.__check_coords(coords)
+        self._coords = coords
+
+    def __check_coords(self, coords):
+        if not all(type(x) in self._allowed_types for x in coords):
+            raise ValueError('неверный тип координат')
+
+    def get_coords(self):
+        return tuple(self._coords)
+
+    @staticmethod
+    def __is_vector(obj):
+        if not isinstance(obj, Vector):
+            raise TypeError('операнд должен быть объектом класса Vector или любого дочернего от него класса')
+
+    def __check_vector_dims(self, other):
+        if len(self._coords) != len(other.get_coords()):
+            raise TypeError('размерности векторов не совпадают')
+
+    def __make_vector(self, coords):
+        try:
+            return self.__class__(*coords)
+        except ValueError:
+            return Vector(*coords)
+
+    def __add__(self, other):
+        self.__is_vector(other)
+        self.__check_vector_dims(other)
+
+        coords = tuple(a + b for a, b in zip(self._coords, other.get_coords()))
+        return self.__make_vector(coords)
+
+    def __sub__(self, other):
+        self.__is_vector(other)
+        self.__check_vector_dims(other)
+
+        coords = tuple(a - b for a, b in zip(self._coords, other.get_coords()))
+        return self.__make_vector(coords)
+
+
+class VectorInt(Vector):
+    _allowed_types = (int,)
