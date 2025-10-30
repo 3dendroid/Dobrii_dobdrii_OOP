2ceca9d374e7930ad0f191486948fc764ad15227
denis.sazonov <denis.sazonov@mytona.com>
2025-10-30 18:59:44 +0700

    NEXT: feat_2.py

--- 4 Inheritance_polymorphism/4.2 issubclass()/example.py
+++ 4 Inheritance_polymorphism/4.2 issubclass()/example.py
@@ -1,1 +1,20 @@
-# NEXT
+# class Geom:
+#     pass
+#
+#
+# class Line(Geom):
+#     pass
+#
+#
+# g = Geom()
+# l = Line()
+# print(l.__class__)
+
+
+class Vector(list):
+    def __str__(self):
+        return " ".join(map(str, self))
+
+
+v = Vector([1, 2, 3])
+print(v)
