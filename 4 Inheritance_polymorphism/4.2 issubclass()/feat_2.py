5cba6eab9a70fecf514014cceb0045b5a4507bd3
denis.sazonov <denis.sazonov@mytona.com>
2025-11-03 16:22:17 +0700

    NEXT: feat_4.py

--- 4 Inheritance_polymorphism/4.2 issubclass()/feat_2.py
+++ 4 Inheritance_polymorphism/4.2 issubclass()/feat_2.py
@@ -1,23 +1,9 @@
 class SmartPhone:
-
-
     pass
 
 
 class IPhone(SmartPhone):
-
-
     pass
 
+
 phone = IPhone()
-
-# print('isinstance(phone, SmartPhone): ', isinstance(phone, SmartPhone))
-# print('issubclass(IPhone, SmartPhone): ', issubclass(IPhone, SmartPhone))
-# print('issubclass(SmartPhone, IPhone): ', issubclass(SmartPhone, IPhone))
-# print('issubclass(SmartPhone, IPhone): ', issubclass(SmartPhone, IPhone))
-# print('issubclass(IPhone, object): ', issubclass(IPhone, object))
-
-try:
-    print(issubclass(IPhone, object))
-except TypeError:
-    print("issubclass(phone, SmartPhone): Ошибка TypeError при вызове")
