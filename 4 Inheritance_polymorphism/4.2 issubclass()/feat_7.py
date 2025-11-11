b5ca0352473d0d156815d7bef4abf6e00674988b
denis.sazonov <denis.sazonov@mytona.com>
2025-11-11 13:05:10 +0700

    NEXT: folder 4.3

--- 4 Inheritance_polymorphism/4.2 issubclass()/feat_7.py
+++ 4 Inheritance_polymorphism/4.2 issubclass()/feat_7.py
@@ -1,1 +1,21 @@
-# NEXT
+class VideoRating:
+    def __init__(self):
+        self.__rating = 0  # приватный атрибут
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        if type(value) is not int or not (0 <= value <= 5):
+            raise ValueError('неверное присваиваемое значение')
+        self.__rating = value
+
+
+class VideoItem:
+    def __init__(self, title, descr, path):
+        self.title = title
+        self.descr = descr
+        self.path = path
+        self.rating = VideoRating()
