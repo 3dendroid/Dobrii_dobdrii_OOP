fd41ad148360361049cf58cda7132efb2718508a
3dendroid <denkingbad@gmail.com>
2025-11-17 15:01:14 +0700

    NEXT: feat_3.py

--- 4 Inheritance_polymorphism/4.3 super()/feat_2.py
+++ 4 Inheritance_polymorphism/4.3 super()/feat_2.py
@@ -1,1 +1,18 @@
-# NEXT
+class SmartPhone:
+    def call(self):
+        pass
+
+    def get_info(self):
+        return "SmartPhone"
+
+
+class IPhone(SmartPhone):
+    def __init__(self, model):
+        self.model = model
+
+    def get_info(self):
+        return super().get_info() + ": IPhone " + self.model
+
+
+s = SmartPhone()
+print(s.get_info())
