6c6079a6c7a9b5e035959d320dde433ee815afab
3dendroid <denkingbad@gmail.com>
2025-11-25 12:18:48 +0700

    NEXT: feat_7.py

--- 4 Inheritance_polymorphism/4.3 super()/feat_5.py
+++ 4 Inheritance_polymorphism/4.3 super()/feat_5.py
@@ -1,1 +1,40 @@
-# NEXT
+class SellItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class House(SellItem):
+    def __init__(self, name, price, material, square):
+        super().__init__(name, price)
+        self.material = material
+        self.square = square
+
+
+class Flat(SellItem):
+    def __init__(self, name, price, size, rooms):
+        super().__init__(name, price)
+        self.size = size
+        self.rooms = rooms
+
+
+class Land(SellItem):
+    def __init__(self, name, price, square):
+        super().__init__(name, price)
+        self.square = square
+
+
+class Agency:
+    def __init__(self, name):
+        self.name = name
+        self._objects = []
+
+    def add_object(self, obj):
+        self._objects.append(obj)
+
+    def remove_object(self, obj):
+        if obj in self._objects:
+            self._objects.remove(obj)
+
+    def get_objects(self):
+        return self._objects
