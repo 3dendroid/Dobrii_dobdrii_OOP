ea4ef616b6f776fff179a513507310f4dd4a53a1
3dendroid <denkingbad@gmail.com>
2025-12-03 12:32:46 +0700

    NEXT: folder 4.4

--- 4 Inheritance_polymorphism/4.3 super()/feat_9.py
+++ 4 Inheritance_polymorphism/4.3 super()/feat_9.py
@@ -1,1 +1,17 @@
-# NEXT
+class StringDigit(str):
+    def __new__(cls, string):
+        if not string.isdigit():
+            raise ValueError("в строке должны быть только цифры")
+        return super().__new__(cls, string)
+
+    def __add__(self, other):
+        new_str = super().__str__() + str(other)
+        if not new_str.isdigit():
+            raise ValueError("в строке должны быть только цифры")
+        return StringDigit(new_str)
+
+    def __radd__(self, other):
+        new_str = str(other) + super().__str__()
+        if not new_str.isdigit():
+            raise ValueError("в строке должны быть только цифры")
+        return StringDigit(new_str)
