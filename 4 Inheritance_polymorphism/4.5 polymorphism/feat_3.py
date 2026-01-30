8baecd51718c103c06e123b605b61bdca62a09c7
3dendroid <denkingbad@gmail.com>
2026-01-30 12:35:59 +0100

    NEXT: feat_4.py

--- 4 Inheritance_polymorphism/4.5 polymorphism/feat_3.py
+++ 4 Inheritance_polymorphism/4.5 polymorphism/feat_3.py
@@ -1,1 +1,14 @@
-# NEXT
+class Lector(Mentor):
+    def set_mark(self, student, mark):
+        student.add_lect_marks(mark)
+
+    def __str__(self):
+        return f"Лектор {self._fio}: предмет {self._subject}"
+
+
+class Reviewer(Mentor):
+    def set_mark(self, student, mark):
+        student.add_house_marks(mark)
+
+    def __str__(self):
+        return f"Эксперт {self._fio}: предмет {self._subject}"
