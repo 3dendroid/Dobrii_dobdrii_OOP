10e435b289d420122b271ed72e503d23c80156b1
3dendroid <denkingbad@gmail.com>
2026-02-23 09:53:43 +0100

    NEXT: feat_2.py

--- 4 Inheritance_polymorphism/4.7 slots/example.py
+++ 4 Inheritance_polymorphism/4.7 slots/example.py
@@ -1,1 +1,35 @@
-# NEXT
+import timeit
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def calc(self):
+        self.x += 1
+        del self.y
+        self.y = 0
+
+
+class Point2D:
+    __slots__ = ('x', 'y')
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def calc(self):
+        self.x += 1
+        del self.y
+        self.y = 0
+
+
+p = Point(1, 2)
+p2 = Point2D(10, 20)
+
+t1 = timeit.timeit(p.calc)
+t2 = timeit.timeit(p2.calc)
+
+print(t1)
+print(t2)
