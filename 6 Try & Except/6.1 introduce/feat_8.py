5144dc0bdb826e9a368d599fb26caa6f59abe939
3dendroid <denkingbad@gmail.com>
2026-04-14 12:14:13 +0200

    NEXT: folder 6.2

--- 6 Try & Except/6.1 introduce/feat_8.py
+++ 6 Try & Except/6.1 introduce/feat_8.py
@@ -1,1 +1,15 @@
-# NEXT
+lst_in = "1 -5.6 True abc 0 23.56 hello".split()
+
+
+def is_digit(x):
+    try:
+        return int(x)
+    except:
+        try:
+            return float(x)
+        except:
+            return x
+
+
+lst_out = list(map(is_digit, lst_in))
+print(lst_out)
---
+++ 6 Try & Except/6.1 introduce/feat_9.py
@@ -0,0 +1,24 @@
+class Triangle:
+    def __init__(self, a, b, c):
+        if type(a) not in (int, float) or type(b) not in (int, float) or type(c) not in (int, float):
+            raise TypeError('стороны треугольника должны быть положительными числами')
+        if a <= 0 or b <= 0 or c <= 0:
+            raise TypeError('стороны треугольника должны быть положительными числами')
+        if a > b + c or b > a + c or c > a + b + c:
+            raise ValueError('из указанных длин сторон нельзя составить треугольник')
+
+        self._a = a
+        self._b = b
+        self._c = c
+
+
+input_data = [(1.0, 4.54, 3), ('abc', 1, 2, 3), (-3, 3, 5.2), (4.2, 5.7, 8.7), (True, 3, 5), (7, 4, 6)]
+
+lst_tr = []
+for data in input_data:
+    try:
+        tr = Triangle(*data)
+    except(ValueError, TypeError):
+        pass
+    else:
+        lst_tr.append(tr)
