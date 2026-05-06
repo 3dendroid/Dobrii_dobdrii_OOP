9fef0d7be42ba80709573d6a88f6b8ba632f12e6
3dendroid <denkingbad@gmail.com>
2026-05-06 08:42:33 +0200

    NEXT: feat_2.py

--- 6 Try & Except/6.5 with_manager/example.py
+++ 6 Try & Except/6.5 with_manager/example.py
@@ -1,1 +1,27 @@
-# NEXT
+class DefenedVector:
+    def __init__(self, v):
+        self.__v = v
+
+    def __enter__(self):
+        self.__temp = self.__v[:]
+        return self.__temp
+
+    def __exit__(self, exc_type):
+        if exc_type is None:
+            self.__v[:] = self.__temp
+
+        return False
+
+
+v1 = [1, 2, 3]
+v2 = [3, 5, 7]
+
+
+try:
+    with DefenedVector(v1) as dv:
+        for i,a in enumerate(dv):
+            dv[i] += v2[i]
+except:
+    print("Error")
+
+print(v1)
